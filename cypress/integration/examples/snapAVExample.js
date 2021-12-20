a4e2c96441a5d4bc2ca2da5287432e4a4ef0f1d4
Joey Barsness <joseph.barsness@gmail.com>
2021-12-20 09:01:06 -0600

    created new primeexample page

--- cypress/integration/examples/snapAVExample.js
+++ cypress/integration/examples/snapAVExample.js
@@ -2,13 +2,14 @@ let accountPageSelectors = require("../../selector.json")["desktop"];
 
 describe('SnapAV Login Test Case Example', function () {
   it('Verifies SnapAV Login Page', function () {
-    cy.visit('https://app.beta.ovrc.com/#/')
-      .get('img')
-      .get('div:nth-child(1) > div.inputWrapper_1-grGzUFUTrbgzX_VnWdA7 > input')
-      .get('div:nth-child(2) > div.inputWrapper_1-grGzUFUTrbgzX_VnWdA7 > input')
-      .get('#button__login')
-      .get('#button__createAccount')
-      .get('#button__forgotPassword');
+    cy.visit('https://app.ovrc.com/#/')
+    cy.get('img').should('include','OvrC')
+    // cy.find("img");
+    // cy.get('div:nth-child(1) > div.inputWrapper_1-grGzUFUTrbgzX_VnWdA7 > input')
+    //   .get('div:nth-child(2) > div.inputWrapper_1-grGzUFUTrbgzX_VnWdA7 > input')
+    //   .get('#button__login')
+    //   .get('#button__createAccount')
+    //   .get('#button__forgotPassword');
   });
   it('Verifies Login to SnapAV', function (){
     cy.get('div:nth-child(1) > div.inputWrapper_1-grGzUFUTrbgzX_VnWdA7 > input').type('joseph.barsness@gmail.com')
